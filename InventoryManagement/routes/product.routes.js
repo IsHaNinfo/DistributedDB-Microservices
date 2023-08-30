608aac5717220a237330e4d7656b3212f891bc83
PrabathAnuradhaDharmasiri <dharmasiripa.20@uom.lk>
2023-08-30 23:01:50 +0530

    Added update and delete

--- InventoryManagement/routes/product.routes.js
+++ InventoryManagement/routes/product.routes.js
@@ -9,6 +9,17 @@ module.exports = app => {
     // Retrieve all products
     router.get("/findall", products.findAll);
   
+    // Retrieve a single products with id
+    router.get("/findbyid/:id", products.findOne);
+  
+    // Update a products with id
+    router.put("/update/:id", products.update);
+  
+    // Delete a products with id
+    router.delete("/deletebyid/:id", products.delete);
+  
+    // Delete all products
+    router.delete("/deleteall", products.deleteAll);
   
     app.use('/api/products', router);
   };
