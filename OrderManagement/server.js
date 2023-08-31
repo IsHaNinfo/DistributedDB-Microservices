3bd40a131f8578194bd94a71f3a79503c833857f
UserSampath <nalakasampathsmp@gmail.com>
2023-08-31 11:00:24 +0530

    add amqp

--- OrderManagement/server.js
+++ OrderManagement/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyparser = require('body-parser');
 const db = require('./models')
 const orderRoutes = require('./routers/OrderRoutes')
+const amqp = require("amqplib");
 
 require('dotenv').config();
 
@@ -12,12 +13,36 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyparser.json());
 
-app.use('/order',orderRoutes)
+app.use('/order', orderRoutes)
 
-db.sequelize.sync({ alter: true }).then((req) =>{
-    app.listen(process.env.PORT,() =>{
+db.sequelize.sync({ alter: true }).then((req) => {
+    app.listen(process.env.PORT, () => {
         console.log(`server up and running on ${process.env.PORT}`)
     })
 })
 
+
+var connection;
+var channel;
+
+async function connect() {
+    const amqpServer = "amqp://localhost:5672";
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue("inventory");
+}
+
+connect().then(() => {
+    channel.consume("inventory", (data) => {
+        console.log("Consuming");
+        const { text } = JSON.parse(data.content);
+        console.log(text);
+        
+        channel.ack(data);
+        channel.sendToQueue(
+            "user",
+            Buffer.from(JSON.stringify({ respond: "sample respond" }))
+        );
+    });
+});
 
