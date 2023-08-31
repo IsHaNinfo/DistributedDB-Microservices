006e14f3e41c701285429a51c40c88d98e81df66
UserSampath <nalakasampathsmp@gmail.com>
2023-08-31 21:51:46 +0530

    configure mssql

--- InventoryManagement/config/db.config.js
+++ InventoryManagement/config/db.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    HOST: "localhost",
-    PORT: "1433", //Depend on your server
-    USER: "sa", //Depend on your server
-    PASSWORD: "12345678", //Depend on your server
-    DB: "products_db",
-    dialect: "mssql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
-  
+  HOST: "localhost",
+  PORT: "1433", //Depend on your server
+  USER: "sa", //Depend on your server
+  PASSWORD: "12345", //Depend on your server
+  DB: "products_db",
+  dialect: "mssql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
