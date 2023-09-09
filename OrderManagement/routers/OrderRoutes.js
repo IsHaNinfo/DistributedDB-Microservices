a307dcc59852dfc2ad0c578d0d669af13971906f
UserSampath <nalakasampathsmp@gmail.com>
2023-09-09 20:30:04 +0530

    remove rabbitMQ configurations for all servers

--- OrderManagement/routers/OrderRoutes.js
+++ OrderManagement/routers/OrderRoutes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const orderRoutes = express.Router();
 const { addOrder, updateOrder, getOrderById, deleteOrder } = require('../controllers/OrderContoller');
 const { or } = require('sequelize');
-const { authenticateUser } = require('../serversHandler');
 
 
-orderRoutes.post('/newOrder', authenticateUser, addOrder);
+orderRoutes.post('/newOrder', addOrder);
 orderRoutes.patch('/updateOrder', updateOrder);
 orderRoutes.get('/userorders', getOrderById);
 orderRoutes.delete('/deleteOrder', deleteOrder);
